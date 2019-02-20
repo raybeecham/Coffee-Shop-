7e3dbd5282ddc2527a5e6c7b06974038e7869ee0
raybeecham <raybeecham2009@gmail.com>
2019-02-20 09:06:06 -0600

    Add files via upload

---
+++ Project2/README for grader.txt
@@ -0,0 +1,26 @@
+This project was created by Cory Singer and Ray Beecham
+
+This is project 2
+
+The way the ADD BUTTON functions is by placing your name
+in the textbox next to Name: such as "Tony Smith", and then placing 
+your Drink in the drink textbox named Drink: such as "Expresso"
+You will then click add order in which your details along with your 
+order ID will be generated.
+
+You may also GET orders from the restlet client by
+using the get button, but will need to place the following endpoint
+http://rest.learncode.academy/api/:csing/:testing.
+
+You will need to place the Order ID obtained from adding in order into 
+the Order ID textbox in order to DELETE or EDIT an order and then 
+clicking the button corresponding to what you are desiring.
+
+We had issues with the Chrome's CORS policy, so we need you to use 
+FireFox. It does work for us on our own machines and
+if you copy the code and use it on your local machine
+then it will work but we are having security issues
+when using it on schools machines or another computer.
+
+Lastly, Cory and I can be reached by our emails on the footer of the page
+if you have any additional questions or problems.
---
+++ Project2/out/artifacts/Project2_war_exploded/_js/main.js
@@ -0,0 +1,100 @@
+$(function(){
+
+    var $name = $("#name");
+    var $drink = $("#drink");
+    var $myOrders = $("#orders");
+    var url = "http://rest.learncode.academy/api/:csing/:testing";
+
+
+
+    function addOrder(order){
+        $myOrders.append("<li>Name: " + order.name + ", Drink: "
+            + order.drink + ", Order ID:" + order.id + "</li>");
+    }
+
+    $("#getOrder").click(function () {
+        showOrders()
+    });
+
+    $("#addOrder").click(function () {
+        var anOrder = {
+            "name": $name.val(),
+            "drink": $drink.val()
+        };
+        $.ajax({
+            type: "POST",
+            url: url,
+            data: anOrder,
+            success: function (newOrder) {
+                addOrder(newOrder)
+            },
+            error: function () {
+                alert("Error saving order");
+            }
+        })
+    });
+
+    $("#clearList").click(function () {
+
+        $.ajax({
+            type: "DELETE",
+            url: url + "/" + $("#id").val(),
+            success: function () {
+                $('#orders').load(document.URL + ' #orderDiv');
+                setTimeout(function (){
+
+                    showOrders();
+
+                }, 10);
+
+            },
+            error: function (result){
+                alert(result.statusText + " " + result.status);
+            }
+        });
+    });
+
+    $("#updateOrder").click(function(){
+
+        var name = $("#name").val();
+        var drink = $("#drink").val();
+
+        $.ajax({
+            type: "PUT",
+            url: url + "/" + $("#id").val(),
+            data: { "name": name, "drink": drink },
+            success: function(){
+
+                $('#orders').load(document.URL + ' #orderDiv');
+                setTimeout(function (){
+
+                    showOrders();
+
+                }, 10);
+            },
+            error: function (result) {
+                console.log("ERROR " + result.statusText + " " + result.status);
+            }});
+
+
+    });
+
+    function showOrders() {
+        $.ajax({
+            type: "GET",
+            url: url,
+            success: function (order) {
+                $.each(order, function (i, order) {
+                    addOrder(order);
+
+                })
+            },
+            error: function () {
+                alert("Please check your URL")
+            }
+        });
+    }
+
+
+
+}); //end of parent function
